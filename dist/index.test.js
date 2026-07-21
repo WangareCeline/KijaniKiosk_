fdc5b12442f9795fa0ae5e48f6b1ca7f51a35c04
Celine Wangare <celinewangare837@gmail.com>
2026-07-21 21:47:53 +0300

    Remove node_modules and dist from tracking, add .gitignore

--- dist/index.test.js
+++
@@ -1,8 +0,0 @@
-const { app } = require('./index');
-const request = require('supertest');
-
-test('GET /health returns 200 and service status', async () => {
-  const res = await request(app).get('/health');
-  expect(res.statusCode).toBe(200);
-  expect(res.body.status).toBe('ok');
-});
